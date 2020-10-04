2dca94673af94835cde1793df57d86a250fa2544
Gabriela Munguia <cursoscomputacion.net@gmail.com>
2020-10-04 13:33:48 -0600

    menu

---
+++ Ejercicios/Proyecto I Unidad/Codigo/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "startShip.h"
+#include "snake.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int opcion = 0;
+
+    
+        system("cls");
+
+        cout << "**************" << endl << endl;
+        cout << " MENU DE JUEGO " << endl;
+        cout << "**************" << endl << endl;
+        cout << "SELECCIONE UN JUEGO: " << endl;
+        cout << "1- startShip" << endl;
+        cout << "2- SNAKE" << endl;
+        cout << "INGRESE LA OPCION PARA JUGAR:" << endl;
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            system("cls");
+              {
+            starShip();
+             }
+            break;
+
+        case 2:
+            system("cls");
+             {
+            snake();
+             }
+            break;
+
+        default:
+            break;
+        }
+    
+    return 0;
+}
